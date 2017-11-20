a0764c659c96261c297f53ed3dea5b98fd6034a2
Marko <criking92@gmail.com>
2017-11-20 21:01:47 +0100

    Izmjena u API config fileu, layout view-u, API kontroleru.

--- TelefonskiImenik/App_Start/WebApiConfig.cs
+++ TelefonskiImenik/App_Start/WebApiConfig.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace TelefonskiImenik
 {
     public static class WebApiConfig
     {
+
+        public class CustomJsonFormatter : JsonMediaTypeFormatter   //kada se želi prikazivati samo JSON u browseru
+        {
+            public CustomJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            }
+
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+        }
+
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -19,6 +37,8 @@ namespace TelefonskiImenik
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Add(new CustomJsonFormatter()); //kada se želi prikazivati samo JSON u browseru
         }
     }
 }
--- TelefonskiImenik/Controllers/API/KontaktController.cs
+++ TelefonskiImenik/Controllers/API/KontaktController.cs
@@ -4,24 +4,97 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 using TelefonskiImenik.Models;
 
 namespace TelefonskiImenik.Controllers.API
 {
     public class KontaktController : ApiController
     {
+        /*---------------------------------------------------------------------------------------------------*/
         private ApplicationDbContext _context;
-
+        /*---------------------------------------------------------------------------------------------------*/
         public KontaktController()
         {
             _context = new ApplicationDbContext();
         }
 
-        public IEnumerable<Osoba> Get()
+        /*---------------------------------------------------------------------------------------------------*/
+        [Route("api/Kontakt/GetBroj/{id}")]
+        [HttpGet]
+        public HttpResponseMessage GetBroj([FromUri]int id)
         {
-            return _context.Osobe.ToList();
+            var broj = (from brojevi in _context.BrojeviOsobe
+                        join brojtip in _context.BrojTipovi on brojevi.BrojTipId equals brojtip.BrojTipId
+                        where brojevi.OsobaId == id
+                        select new
+                        {
+                            Broj = brojevi.Broj,
+                            BrojTip = brojtip.Naziv,
+                            OpisBroja = brojevi.Opis
+                        }).ToList();
+
+            if (broj != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, broj);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Zapis ne postoji u bazi podataka.");
+            }
         }
 
+        /*---------------------------------------------------------------------------------------------------*/
+        [Route("api/Kontakt/GetOsobe")]
+        [HttpGet]
+        public HttpResponseMessage GetOsobe()
+        {
+            var osoba = from oso in _context.Osobe
+                        group oso by new
+                        {
+                            oso.Ime,
+                            oso.Prezime
+                        } into os
+                        select os.FirstOrDefault();
+            return Request.CreateResponse(HttpStatusCode.OK, osoba);
+        }
+
+        /*---------------------------------------------------------------------------------------------------*/
+        [Route("api/Kontakt/GetOsoba/{id}")]
+        [HttpGet]
+        public HttpResponseMessage GetOsoba(int id)
+        {
+            var osoba = _context.Osobe.Where(x => x.OsobaId == id).Select(x => new { x.Ime, x.Prezime, x.Grad, x.Opis }).ToList();
+            if (osoba != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, osoba);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Osoba ne postoji u bazi podataka.");
+            }
+        }
+
+        /*---------------------------------------------------------------------------------------------------*/
+        [Route("api/Kontakt/DodajBroj")]
+        [HttpPost]
+        public HttpResponseMessage DodajBroj([FromBody]BrojeviOsoba broj)
+        {
+            try
+            {
+                _context.BrojeviOsobe.Add(broj);
+                _context.SaveChanges();
+
+                return Request.CreateResponse(HttpStatusCode.Created, broj);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        /*---------------------------------------------------------------------------------------------------*/
         [Route("api/Kontakt/DodajOsobu")]
         [HttpPost]
         public HttpResponseMessage DodajOsobu([FromBody]Osoba osoba)
@@ -31,29 +104,7 @@ namespace TelefonskiImenik.Controllers.API
                 _context.Osobe.Add(osoba);
                 _context.SaveChanges();
 
-                //return Json(new { id = osoba.OsobaId });
-                var message = Request.CreateResponse(HttpStatusCode.Created, osoba);
-                message.Headers.Location = new Uri(Request.RequestUri + osoba.OsobaId.ToString());
-                return message;
-            }
-            catch (Exception ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
-            }
-        }
-
-        [Route("api/Kontakt/DodajBroj")]
-        //POST: api/Kontakt
-        [HttpPost]
-        public HttpResponseMessage DodajBroj([FromBody]BrojeviOsoba broj)
-        {
-            try
-            {
-                _context.BrojeviOsobe.Add(broj);
-                _context.SaveChanges();
-
-                var message = Request.CreateResponse(HttpStatusCode.Created, broj);
-                return message;
+                return Request.CreateResponse(HttpStatusCode.Created, osoba);
             }
             catch (Exception ex)
             {
@@ -62,3 +113,5 @@ namespace TelefonskiImenik.Controllers.API
         }
     }
 }
+
+
